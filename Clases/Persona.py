78403a23ea094404e983c7dc2312f732193acf90
Agustin David  Hoyos <agustindavidhoyos@gmail.com>
2025-11-04 17:53:19 -0300

    clase persona y clase cliente
    cree la clase persona con sus atributos dni es privado por que es unico de la clase persona
    
    y cree las clases cliente tambien con sus conexiones

--- Clases/Persona.py
+++ Clases/Persona.py
@@ -1,2 +1,27 @@
+import sqlite3
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "SalaDeCine_DB.db")
 class Persona:
-    jashdkjad
+    def __init__(self,dni,nombre,apellido,email):
+        self.__dni = dni 
+        self.nombre = nombre 
+        self.apellido = apellido
+        self.email = email
+        
+    staticmethod
+    def _get_connection():
+        return sqlite3.connect(DB_PATH)
+
+    @property
+    def dni(self):
+        return self.__dni
+    
+    @dni.setter
+    def dni(self,nuevo_dni):
+        self.__dni = nuevo_dni
+
+
+
+
+
