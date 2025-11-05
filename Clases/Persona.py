1e17d34913c00e4e6b19e112cce5cec5c99a1b20
Moksys1 <ariel21roldan@gmail.com>
2025-11-05 20:20:29 -0300

    Clase persona corregida

--- Clases/Persona.py
+++ Clases/Persona.py
@@ -1,7 +1,9 @@
 import sqlite3
 import os
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "..", "SalaDeCine_DB.db")
+
 class Persona:
     def __init__(self,dni,nombre,apellido,email):
         self.__dni = dni 
@@ -9,10 +11,6 @@ class Persona:
         self.apellido = apellido
         self.email = email
         
-    staticmethod
-    def _get_connection():
-        return sqlite3.connect(DB_PATH)
-
     @property
     def dni(self):
         return self.__dni
