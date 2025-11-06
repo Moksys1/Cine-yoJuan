813302160e8a9adb95b0127f0d15399d571ac8f2
Moksys1 <ariel21roldan@gmail.com>
2025-11-05 21:20:51 -0300

    Clase Persona Corregida nuevamente
    Hice los atributos publicos para no complicarnos, borre la conexion a la base de datos ya que no la necesita y adapte los atributos a la BD y UML que venimos trabajando

--- Clases/Persona.py
+++ Clases/Persona.py
@@ -1,20 +1,5 @@
-import sqlite3
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "..", "SalaDeCine_DB.db")
-
 class Persona:
-    def __init__(self,dni,nombre,apellido,email):
-        self.__dni = dni 
+    def __init__(self, dni, nombre, email):
+        self.dni = dni 
         self.nombre = nombre 
-        self.apellido = apellido
         self.email = email
-        
-    @property
-    def dni(self):
-        return self.__dni
-    
-    @dni.setter
-    def dni(self,nuevo_dni):
-        self.__dni = nuevo_dni
