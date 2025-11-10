76e25661f109a3e209d37fd469379b5ac09b7ce2
Moksys1 <ariel21roldan@gmail.com>
2025-11-10 14:38:43 -0300

    Clase Funcion corregida
    Cree la clase funcion con su respectivo constructor y conexion a BD.

--- Clases/Funcion.py
+++ Clases/Funcion.py
@@ -1,37 +1,31 @@
 import sqlite3
-from datetime import date,time
+import os
+from datetime import datetime
+from Pelicula import Pelicula
+from Sala import Sala
 
-
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "SalaDeCine_DB.db")
 
 class Funcion:
-    def __init__(self,id_funcion:int, fecha: date,hora:time,precio:float):
-        self.id_funcion=id_funcion
-        self.fecha=fecha
-        self.hora=hora
-        self.precio=precio 
-    def __str__(self):      #devuelve una cadena legible al imprimir el objeto
-       return f"Funcion {self.id_funcion}:{self.fecha} {self.hora}- ${self.precio:.2f}"
+    def __init__(self, id_funcion=None, pelicula=None, sala=None, fecha_hora=None, idioma="2D", formato="Español", precio_final= 0.0):
+        self.id_funcion = id_funcion
+        self.pelicula = pelicula
+        self.sala = sala
+        self.fecha_hora = fecha_hora or datetime.now()
+        self.idioma = idioma
+        self.formato = formato
+        self.precio_final = self.sala.precioBase if sala else 0.0
 
+        idiomas_validos = ["Español", "Ingles(Subtitulada)"]
+        formatos_validos = ["2D", "3D"]
 
-#creacion de base de datos 
-conn = sqlite3.connect('cine.db')
-cursor=conn.cursor()
-cursor.execute("""
-    create table if not exists Funcion
-        id_funcion interger primary key autoincrement ,
-        fun_fecha date ,
-        fun_hora time ,
-        fun_precio float
-            """)
-conn.commit()  
-conn.close()
-print("la tabla funcion fue creada con exito")
+        if self.idioma not in idiomas_validos:
+            raise ValueError(f"Idioma inválido. Debe ser uno de: {idiomas_validos}")
+        if self.formato not in formatos_validos:
+            raise ValueError(f"Formato inválido. Debe ser uno de: {formatos_validos}")
+        
+    @staticmethod
+    def _get_connection():
+        return sqlite3.connect(DB_PATH)
 
-
-# def getDisponibilidad(self):  
-
-
-#     if total == 0:
-#         return "no hay butacas disponibles"
-#     return f"asientos disponibles:
