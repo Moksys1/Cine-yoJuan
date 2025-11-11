4acb3559d590e83778df1f319ce5717198ab9b14
Moksys1 <ariel21roldan@gmail.com>
2025-11-11 01:48:29 -0300

    Update Butaca.py
    Conexion a BD en la Clase Butaca

--- Clases/Butaca.py
+++ Clases/Butaca.py
@@ -1,3 +1,9 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "SalaDeCine_DB.db")
+
 class Butaca:
     def __init__(self, id_butaca=None, fila="", numero=0, id_sala=None, ocupada=False):
        self.id_butaca = id_butaca
@@ -5,16 +11,30 @@ class Butaca:
        self.numero = numero 
        self.id_sala = id_sala
        self.ocupada = ocupada
+
+    @staticmethod
+    def _get_connection():
+        return sqlite3.connect(DB_PATH)
     
-    def reservar(self):
+    def ocupar(self):
         if not self.ocupada:
             self.ocupada = True
+            conexion = self._get_connection()
+            cursor = conexion.cursor()
+            cursor.execute("UPDATE Butaca SET disponibilidad = 1 WHERE idButaca = ?", (self.id_butaca,))
+            conexion.commit()
+            conexion.close()
             return True
         return False
 
     def liberar(self):
         if self.ocupada:
             self.ocupada = False
+            conexion = self._get_connection()
+            cursor = conexion.cursor()
+            cursor.execute("UPDATE Butaca SET disponibilidad = 0 WHERE idButaca = ?", (self.id_butaca,))
+            conexion.commit()
+            conexion.close()
             return True
         return False
 
