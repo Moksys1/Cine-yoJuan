ebd1825714505a4691855e0a980d8f8ab57137a4
Moksys1 <ariel21roldan@gmail.com>
2025-11-10 21:17:24 -0300

    Update Butaca.py

--- Clases/Butaca.py
+++ Clases/Butaca.py
@@ -1,25 +1,25 @@
 class Butaca:
-    def __init__(self, id_butaca=None, fila="", numero=0, id_sala=None, reservada=False):
+    def __init__(self, id_butaca=None, fila="", numero=0, id_sala=None, ocupada=False):
        self.id_butaca = id_butaca
        self.fila = fila
        self.numero = numero 
        self.id_sala = id_sala
-       self.reservada = reservada
+       self.ocupada = ocupada
     
     def reservar(self):
-        if not self.reservada:
-            self.reservada = True
+        if not self.ocupada:
+            self.ocupada = True
             return True
         return False
 
     def liberar(self):
-        if self.reservada:
-            self.reservada = False
+        if self.ocupada:
+            self.ocupada = False
             return True
         return False
 
     def __str__(self):
-        estado = "Reservada" if self.reservada else "Libre"
+        estado = "Ocupada" if self.ocupada else "Libre"
         return f"butaca {self.fila}{self.numero}: ({estado})"
 
    
